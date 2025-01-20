cf4fb397d62a5aabd563d44f1c7b029bc9102226
TheHiDId <eope1318@gmail.com>
2025-01-20 11:25:11 +0900

    20250120_Java초기설정

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,24 @@
+// JS와 주석 동일
+
+/* 
+ * Run 버튼 클릭 시 (단축키 ctrl + f11)
+ * 1. 자동으로 컴파일 수행 (javac OOO.java)
+ * 	-> .class 파일 (byte code) 생성
+ * 
+ * 2. 컴파일 완료 후 .class 파일 실행 (java class 파일명)
+ */
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("이클립스 실행 중");
+		
+		/*
+		 * Java API DOC: 자바에서 제공해주는 코드의 사용법이 설명되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1. Java 21 API 검색
+		 * 
+		 * 2. 궁금한 코드를 클릭하고 shift + f2
+		 */
+	}
+}
